c2fb8ac89f214c5fc42310215b8ffbcc4ff273e4
Uzair <s2023065101@umt.edu.pk>
2026-01-26 11:16:27 +0500

    Auto create superuser for Render free tier

--- build.sh
+++ build.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+# AUTO CREATE SUPERUSER (ONLY IF NOT EXISTS)
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+CNIC = "3540487138783"
+PASSWORD = "Admin@123"
+FULL_NAME = "Admin User"
+CONSTITUENCY = "ADMIN"
+
+if not User.objects.filter(cnic=CNIC).exists():
+    User.objects.create_superuser(
+        cnic=CNIC,
+        password=PASSWORD,
+        full_name=FULL_NAME,
+        constituency=CONSTITUENCY
+    )
+    print("Superuser created")
+else:
+    print("Superuser already exists")
+EOF
