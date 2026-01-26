4dc2e51df2db78c81a0fd036cc4a6cf0e7ef843b
Uzair <s2023065101@umt.edu.pk>
2026-01-26 11:12:53 +0500

    Configure PostgreSQL database on Render

--- evoting_core/settings.py
+++ evoting_core/settings.py
@@ -87,12 +87,17 @@ WSGI_APPLICATION = 'evoting_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
